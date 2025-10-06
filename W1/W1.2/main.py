ed3562be9ef66dba8ddd42a475b7acdce4ee1fc9
Joni Jussila <jonijussila123@gmail.com>
2025-10-07 00:07:24 +0300

    Add second task files to W1.2

---
+++ W1/W1.2/main.py
@@ -0,0 +1,8 @@
+# Ask user for their name
+name = input("Enter your name: ")
+
+# Check the name and print appropriate message
+if name == "Clark Kent":
+    print("You are the Superman!")
+else:
+    print("You are an ordinary person.")
